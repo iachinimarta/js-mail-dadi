bdd42d8a3ba6409dbd94c5d317482ea525968b66
Marta Iachini <iachinimarta@gmail.com>
2022-07-07 15:48:26 +0200

    Aggiunta soluzione abbreviata in progetto mail

--- mail/js/main.js
+++ mail/js/main.js
@@ -12,20 +12,22 @@ for (let i = 0; i < list.length; i++) {
     console.log(i);
     console.log(typeof i);
 
-    if (i == email0) {
-        alert("Accesso consentito!");
-    } else if (i == email1) {
-        alert("Accesso consentito!");
-    } else if (i == email2) {
+    if ((i == email0) || (i == email1) || (i == email2)) {
         alert("Accesso consentito!");
     } else {
         alert("Accesso negato!");
     }
 
-        // if (i = list) {
-        //     alert("Accesso consentito!");
-        // } else if (i != list) {
-        //     alert("Accesso negato!");
-        // }
+    // VERSIONE ESTESA:
+    
+    //  if (i == email0) {
+    //     alert("Accesso consentito!");
+    // } else if (i == email1) {
+    //     alert("Accesso consentito!");
+    // } else if (i == email2) {
+    //     alert("Accesso consentito!");
+    // } else {
+    //     alert("Accesso negato!");
+    // }
 }
 
