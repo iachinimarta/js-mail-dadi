1454fe7bd00eebc8d2c3a9390c8588aec558dc77
Marta Iachini <iachinimarta@gmail.com>
2022-07-07 15:26:01 +0200

    Prima soluzione al progetto mail

---
+++ mail/js/main.js
@@ -0,0 +1,31 @@
+const email0 = "nome@email.it";
+const email1 = "nome1@email.it";
+const email2 = "nome2@email.it";
+
+const list = [ email0, email1, email2];
+console.log(list);
+
+let addEmail = prompt("Inserisci la tua email..");
+
+for (let i = 0; i < list.length; i++) {
+    i = addEmail;
+    console.log(i);
+    console.log(typeof i);
+
+    if (i == email0) {
+        alert("Accesso consentito!");
+    } else if (i == email1) {
+        alert("Accesso consentito!");
+    } else if (i == email2) {
+        alert("Accesso consentito!");
+    } else {
+        alert("Accesso negato!");
+    }
+
+        // if (i = list) {
+        //     alert("Accesso consentito!");
+        // } else if (i != list) {
+        //     alert("Accesso negato!");
+        // }
+}
+
